4226d7f101d269d420b5e97059448aa7f144a834
Stanislav Zaleski <sigmaray@gmail.com>
2024-03-08 17:29:50 +0500

    заготовка

---
+++ my/json-parse/main.c
@@ -0,0 +1,113 @@
+// https://zetcode.com/gui/gtk2/firstprograms/
+
+#include <gtk/gtk.h>
+#include <cjson/cJSON.h>
+
+char *create_monitor(void)
+{
+    const unsigned int resolution_numbers[3][2] = {
+        {1280, 720},
+        {1920, 1080},
+        {3840, 2160}
+    };
+    char *string = NULL;
+    cJSON *name = NULL;
+    cJSON *resolutions = NULL;
+    cJSON *resolution = NULL;
+    cJSON *width = NULL;
+    cJSON *height = NULL;
+    size_t index = 0;
+
+    cJSON *monitor = cJSON_CreateObject();
+    if (monitor == NULL)
+    {
+        goto end;
+    }
+
+    name = cJSON_CreateString("Awesome 4K");
+    if (name == NULL)
+    {
+        goto end;
+    }
+    /* after creation was successful, immediately add it to the monitor,
+     * thereby transferring ownership of the pointer to it */
+    cJSON_AddItemToObject(monitor, "name", name);
+
+    resolutions = cJSON_CreateArray();
+    if (resolutions == NULL)
+    {
+        goto end;
+    }
+    cJSON_AddItemToObject(monitor, "resolutions", resolutions);
+
+    for (index = 0; index < (sizeof(resolution_numbers) / (2 * sizeof(int))); ++index)
+    {
+        resolution = cJSON_CreateObject();
+        if (resolution == NULL)
+        {
+            goto end;
+        }
+        cJSON_AddItemToArray(resolutions, resolution);
+
+        width = cJSON_CreateNumber(resolution_numbers[index][0]);
+        if (width == NULL)
+        {
+            goto end;
+        }
+        cJSON_AddItemToObject(resolution, "width", width);
+
+        height = cJSON_CreateNumber(resolution_numbers[index][1]);
+        if (height == NULL)
+        {
+            goto end;
+        }
+        cJSON_AddItemToObject(resolution, "height", height);
+    }
+
+    string = cJSON_Print(monitor);
+    if (string == NULL)
+    {
+        fprintf(stderr, "Failed to print monitor.\n");
+    }
+
+end:
+    cJSON_Delete(monitor);
+    return string;
+}
+
+int main(int argc, char *argv[]) {
+    
+  GtkWidget *window;
+  GtkWidget *view;
+  GtkWidget *vbox;
+  
+  GtkTextBuffer *buffer;
+
+  gtk_init(&argc, &argv);
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "JSON");
+  gtk_window_set_default_size(GTK_WINDOW(window), 230, 150);
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+  vbox = gtk_vbox_new(FALSE, 0);
+  view = gtk_text_view_new();
+  gtk_box_pack_start(GTK_BOX(vbox), view, TRUE, TRUE, 0);
+  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
+  // gtk_text_buffer_set_text (buffer, "Hello, this is some text", -1);
+  char *monitor = create_monitor();
+  // printf("\"%s\"", monitor);
+  gtk_text_buffer_set_text (buffer, monitor, -1);
+
+  gtk_container_add(GTK_CONTAINER(window), vbox);
+
+  // gtk_widget_show(window);
+  gtk_widget_show_all(window);
+
+  g_signal_connect(G_OBJECT(window), "destroy",
+      G_CALLBACK(gtk_main_quit), NULL);
+
+  gtk_main();
+
+  return 0;
+}
