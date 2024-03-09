6ce89eb28edbcb909ad419dfa3a7b48e3fcda729
Stanislav Zaleski <sigmaray@gmail.com>
2024-03-09 14:18:59 +0500

    .

--- my/download-url/main.c
+++ my/download-url/main.c
@@ -2,6 +2,7 @@
 
 #include <gtk/gtk.h>
 #include <cjson/cJSON.h>
+#include "download_temp.c"
 
 // int main(int argc, char *argv[]) {
 
@@ -16,26 +17,6 @@
 // }
 
 int main() {
-//   const gchar *url = "http://cdsweb.u-strasbg.fr/cgi-bin/nph-sesame/-oI/A?M45";
-  const gchar *url = "https://api.openweathermap.org/data/2.5/weather?q=Berlin&lang=ru&units=metric&appid=5a043a1bd95bf3ee500eb89de107b41e";
-  GFile *file = g_file_new_for_uri(url);
-  GError *error = NULL;
-  gchar *content = NULL;
-
-  if (!g_file_load_contents(file, NULL, &content, NULL, NULL, &error)) {
-    g_error("%s\n", error->message);
-    g_clear_error(&error);
-  }
-  g_object_unref(file);
-	
-  // g_printf("%s\n", content);
-  cJSON *parsed_json = cJSON_Parse(content);
-  char *printed_json = cJSON_Print(parsed_json);
-  g_printf("%s\n", printed_json);
-
-  const cJSON *main = NULL;
-  main = cJSON_GetObjectItemCaseSensitive(parsed_json, "main");
-  cJSON *temp = cJSON_GetObjectItemCaseSensitive(main, "temp");
-  int tempVal = temp->valueint;
-
+  int temp = download_temp();
+  printf("temp: %d\n", temp);
 }
